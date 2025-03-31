09a6d0b686fa3c6115006ebcd8dbb4c2275c14bc
Алина <alissha.p@yandex.ru>
2025-04-01 00:21:55 +0500

    Добавление «выхода» и его настройка

--- robots/src/gui/RobotsProgram.java
+++ robots/src/gui/RobotsProgram.java
@@ -1,25 +1,26 @@
 package gui;
 
 import java.awt.Frame;
+import java.util.Locale;
+import javax.swing.*;
 
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
+public class RobotsProgram {
+  public static void main(String[] args) {
+    try {
+      Locale.setDefault(new Locale("ru", "RU"));
+      UIManager.put("OptionPane.yesButtonText", "Да");
+      UIManager.put("OptionPane.noButtonText", "Нет");
 
-public class RobotsProgram
-{
-    public static void main(String[] args) {
-      try {
-        UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-//        UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-//        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-//        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-      SwingUtilities.invokeLater(() -> {
-        MainApplicationFrame frame = new MainApplicationFrame();
-        frame.pack();
-        frame.setVisible(true);
-        frame.setExtendedState(Frame.MAXIMIZED_BOTH);
-      });
-    }}
+      UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    SwingUtilities.invokeLater(() -> {
+      MainApplicationFrame frame = new MainApplicationFrame();
+      frame.pack();
+      frame.setVisible(true);
+      frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+    });
+  }
+}
